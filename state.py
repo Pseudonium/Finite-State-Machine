672b2b17be18988929ba5beb099d7ad2e5353805
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2021-03-01 14:21:38 +0000

    Added rudimentary expected time algorithm

--- state.py
+++ state.py
@@ -59,6 +59,27 @@ class FiniteStateMachine:
                 if not finished: # I.e. never found a matching node
                     node["arrows"][outcome] = 0 # Back to the start
 
+    def expected_time(self):
+        self.compute_connections()
+        # Let T_n be the expected time taken to reach node n from the start
+        # So, T_0 = 0, and want to find e.g. T_m
+        # Well, what's the recurrence relation?
+        # Let's say we know up to T_n, and want T_{n + 1}
+        # Well, say starting from node n
+        # Then, reach T_{n + 1} with probability 1/2
+        # Or go back to node k with probability 1/2
+        # Then, node that T_b - T_a = "time to reach a from b", for a > b
+        # So, T_{n + 1} - T_n = 1/2 (1) + 1/2(1 + T_{n + 1} - T_k)
+        # Re-arrange to get T_{n + 1} = 2 + 2 T_n - T_k
+        # So, can iteratively compute this, starting from T_0 = 0, to find T_m!
+        times_from_start = [0]
+        for index, node in enumerate(self.nodes[:-1]):
+            fail_idx = min(node["arrows"].values())
+            times_from_start.append(
+                2 + 2*times_from_start[index] - times_from_start[fail_idx]
+            )
+        return times_from_start[-1]
+
     def __repr__(self):
         result = ""
         for node in self.nodes:
@@ -67,6 +88,5 @@ class FiniteStateMachine:
                 result += outcome + ":" + self.nodes[pointer]["name"] + "\n"
         return result
 
-y = FiniteStateMachine("HTTHHHTH")
-y.compute_connections()
-print(y)
+y = FiniteStateMachine("THTHTTHT")
+print(y.expected_time())
